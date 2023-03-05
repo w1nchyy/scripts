43fe6d3a30133a060793af9986a1f5b9ed356f2d
w1nchyy <127020279+w1nchyy@users.noreply.github.com>
2023-03-05 14:47:21 +0300

    Create fps-booster.lua

---
+++ fps-booster.lua
@@ -0,0 +1,24 @@
+_G.Settings = {
+    Players = {
+        ["Ignore Me"] = false, -- Ignore your Character
+        ["Ignore Others"] = false -- Ignore other Characters
+    },
+    Meshes = {
+        Destroy = true, -- Destroy Meshes
+        LowDetail = true -- Low detail meshes (NOT SURE IT DOES ANYTHING)
+    },
+    Images = {
+        Invisible = true, -- Invisible Images
+        LowDetail = true, -- Low detail images (NOT SURE IT DOES ANYTHING)
+        Destroy = true, -- Destroy Images
+    },
+    ["No Particles"] = true, -- Disables all ParticleEmitter, Trail, Smoke, Fire and Sparkles
+    ["No Camera Effects"] = true, -- Disables all PostEffect's (Camera/Lighting Effects)
+    ["No Explosions"] = true, -- Makes Explosion's invisible
+    ["No Clothes"] = true, -- Removes Clothing from the game
+    ["Low Water Graphics"] = true, -- Removes Water Quality
+    ["No Shadows"] = true, -- Remove Shadows
+    ["Low Rendering"] = true, -- Lower Rendering
+    ["Low Quality Parts"] = true -- Lower quality parts
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/FPSBooster.lua"))()
